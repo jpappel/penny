618f25fb6fbe3be43d5a568f0317a6638594f026
JP Appel <jeanpierre.appel01@gmail.com>
2025-01-13 11:46:55 -0500

    Add methods for getting root comments and children comments

--- data/query.go
+++ data/query.go
@@ -12,7 +12,7 @@ func parseComment(result *sql.Rows, unixTime int64) (Comment, error) {
 	var hiddenTime sql.NullInt64
 	var deletedTime sql.NullInt64
 	var postedTime int64
-	if err := result.Scan(&comment.Id, &hiddenTime, &deletedTime, &postedTime, &(comment.Content)); err != nil {
+	if err := result.Scan(&comment.Id, &hiddenTime, &deletedTime, &postedTime, &comment.Content, &comment.NumChildren); err != nil {
 		return Comment{}, nil
 	}
 
@@ -30,11 +30,12 @@ func parseComment(result *sql.Rows, unixTime int64) (Comment, error) {
 
 func (p PennyDB) GetPageCommentsById(ctx context.Context, pageId int) ([]Comment, error) {
 	now := time.Now().UTC().Unix()
-	result, err := p.db.QueryContext(ctx, `SELECT id, hiddenTime, deletedTime, postedTime, content
+	result, err := p.db.QueryContext(ctx, `SELECT id, hiddenTime, deletedTime, postedTime, content, children
     FROM Comments
+    JOIN Descendants ON Comments.id = Descendants.commentId
     WHERE pageId = ? AND (deletedTime > ? OR deletedTime IS NULL)`, pageId, now)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer result.Close()
 
@@ -42,7 +43,7 @@ func (p PennyDB) GetPageCommentsById(ctx context.Context, pageId int) ([]Comment
 	for result.Next() {
 		comment, err := parseComment(result, now)
 		if err != nil {
-			panic(err)
+			return comments, err
 		}
 		comments = append(comments, comment)
 	}
@@ -52,12 +53,13 @@ func (p PennyDB) GetPageCommentsById(ctx context.Context, pageId int) ([]Comment
 
 func (p PennyDB) GetPageComments(ctx context.Context, pageUrl string) ([]Comment, error) {
 	now := time.Now().UTC().Unix()
-	result, err := p.db.QueryContext(ctx, `SELECT Comments.id, hiddenTime, deletedTime, postedTime, content
+	result, err := p.db.QueryContext(ctx, `SELECT Comments.id, hiddenTime, deletedTime, postedTime, content, children
     FROM Comments
     JOIN Pages ON Comments.pageId = Pages.id
-    WHERE url = ? AND (deletedTime > ? OR deletedTime IS NULL)`, pageUrl, now)
+    JOIN Descendants ON Comments.id = Descendants.commentId
+    WHERE url = ?`, pageUrl)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer result.Close()
 
@@ -65,10 +67,94 @@ func (p PennyDB) GetPageComments(ctx context.Context, pageUrl string) ([]Comment
 	for result.Next() {
 		comment, err := parseComment(result, now)
 		if err != nil {
-			panic(err)
+			return comments, err
 		}
 		comments = append(comments, comment)
 	}
 
 	return comments, nil
+}
+
+func (p PennyDB) GetPageRootComments(ctx context.Context, pageUrl string) ([]Comment, error) {
+	now := time.Now().UTC().Unix()
+	// PERF: this query should be optimized to require less joins
+	result, err := p.db.QueryContext(ctx, `SELECT Comments.id, hiddenTime, deletedTime, postedTime, content, children
+    FROM Comments
+    JOIN Pages on Comments.pageId = Pages.id
+    JOIN Descendants ON Comments.id = Descendants.commentId
+    JOIN Relations ON Comments.id = Relations.childId
+    WHERE url = ? AND parentId IS NULL`, pageUrl)
+	if err != nil {
+		return nil, err
+	}
+	defer result.Close()
+
+	comments := make([]Comment, 0)
+	for result.Next() {
+		comment, err := parseComment(result, now)
+		if err != nil {
+			return comments, err
+		}
+		comments = append(comments, comment)
+	}
+
+	return comments, nil
+}
+
+func (p PennyDB) GetCommentById(ctx context.Context, commentId int) (Comment, error) {
+	now := time.Now().UTC().Unix()
+	row := p.db.QueryRowContext(ctx, `SELECT id, hiddenTime, deletedTime, postedTime, content, children
+    FROM Comments JOIN Descendants ON Comments.id = Descendants.commentId WHERE id = ?`, commentId)
+
+	comment := Comment{}
+	var hiddenTime sql.NullInt64
+	var deletedTime sql.NullInt64
+	var postedTime int64
+	if err := row.Scan(&comment.Id, &hiddenTime, &deletedTime, &postedTime, &comment.Content); err != nil {
+		return Comment{}, nil
+	}
+
+	if hiddenTime.Valid {
+		comment.Hidden = hiddenTime.Int64 < now
+	}
+
+	if deletedTime.Valid {
+		comment.Deleted = deletedTime.Int64 < now
+	}
+	comment.Posted = time.Unix(postedTime, 0)
+
+	return comment, nil
+}
+
+// Adds a comments children to it
+func (p PennyDB) GetCommentChildren(ctx context.Context, comment *Comment) error {
+	if comment.NumChildren == 0 {
+		comment.Children = make([]Comment, 0, 5)
+		return nil
+	}
+
+	now := time.Now().UTC().Unix()
+	if comment.Children == nil {
+		comment.Children = make([]Comment, 0, comment.NumChildren)
+	}
+
+	result, err := p.db.QueryContext(ctx, `SELECT id, hiddenTime, deletedTime, postedTime, content, children
+    FROM Comments
+    JOIN Descendants ON Comments.id = Descendants.commentId
+    Join Relations ON Comments.id = Relations.childId
+    WHERE id = ? AND parentId = ?`, comment.Id, comment.Id)
+	if err != nil {
+		return err
+	}
+	defer result.Close()
+
+	for result.Next() {
+		childComment, err := parseComment(result, now)
+		if err != nil {
+			return err
+		}
+		comment.Children = append(comment.Children, childComment)
+	}
+
+	return nil
 }
--- data/types.go
+++ data/types.go
@@ -7,18 +7,19 @@ import (
 )
 
 type Comment struct {
-	Id       int
-	Content  string
-	Hidden   bool
-	Deleted  bool
-	Posted   time.Time
-	Children []Comment
+	Id          int
+	Content     string
+	Hidden      bool
+	Deleted     bool
+	Posted      time.Time
+	NumChildren int
+	Children    []Comment
 }
 
 func (c Comment) String() string {
 	formatStr := "Comment %d: hidden[%t] deleted[%t]\nPosted (UTC) %s\n%d Children\n---\n%s"
 	return fmt.Sprintf(formatStr,
-		c.Id, c.Hidden, c.Deleted, c.Posted.String(), len(c.Children), c.Content)
+		c.Id, c.Hidden, c.Deleted, c.Posted.String(), c.NumChildren, c.Content)
 }
 
 type PennyDB struct {
