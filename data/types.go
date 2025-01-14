ce857cd7f2ee50c4f1dd144e0de3e48d39e7e408
JP Appel <jeanpierre.appel01@gmail.com>
2025-01-14 00:46:47 -0500

    Add hash function to comment

--- data/types.go
+++ data/types.go
@@ -1,7 +1,9 @@
 package data
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"encoding/hex"
 	"fmt"
 	"time"
 )
@@ -20,6 +22,12 @@ func (c Comment) String() string {
 	formatStr := "Comment %d: hidden[%t] deleted[%t]\nPosted (UTC) %s\n%d Children\n---\n%s"
 	return fmt.Sprintf(formatStr,
 		c.Id, c.Hidden, c.Deleted, c.Posted.String(), c.NumChildren, c.Content)
+}
+
+func (c Comment) Hash() string {
+	str := fmt.Sprint(c.Id, c.Content, c.Hidden, c.Deleted, c.Posted.UTC().Unix(), c.NumChildren)
+	hash := sha256.Sum256([]byte(str))
+	return hex.EncodeToString(hash[:])
 }
 
 type PennyDB struct {
